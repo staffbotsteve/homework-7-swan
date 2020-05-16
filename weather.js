f4ce6f428e7cadc9ffc5c3835a02254b78e4a258
heather swan <heatherswan@heathers-MacBook-Air.local>
2020-05-16 13:24:58 -0700

    adding ul, search

--- weather.js
+++ weather.js
@@ -1,5 +1,6 @@
 var apiKey = "11c2ecc6f7da4895be5f0e57b4d438a9";
 var queryBase = "http://api.openweathermap.org/data/2.5/";
+var city="";
 
 function fiveDayForecast(city) {
   days = 5;
@@ -13,4 +14,12 @@ function fiveDayForecast(city) {
     console.log(response);
   });
 }
-fiveDayForecast("Sacramento")
+$('#search').click(function(){
+city = $('#searchInput').val()
+fiveDayForecast(city)
+setCity(city)
+})
+
+function setCity(city){
+    $("#cityList").prepend("<li class=\"nav-item\"><a class=\"nav-link\" href=\"#\">" + city + "<span class=\"city\"></span></a></li>");
+}
