46de46360996f71eb44e9a4792de92796d47120b
heather swan <heatherswan@heathers-MacBook-Air.local>
2020-05-16 14:01:36 -0700

    adding uv index ajax and today weather card

--- weather.js
+++ weather.js
@@ -1,6 +1,8 @@
 var apiKey = "11c2ecc6f7da4895be5f0e57b4d438a9";
 var queryBase = "http://api.openweathermap.org/data/2.5/";
 var city="";
+var lat="";
+var lon="";
 
 function fiveDayForecast(city) {
   days = 5;
@@ -11,9 +13,27 @@ function fiveDayForecast(city) {
     url: queryURL,
     method: "GET",
   }).then(function (response) {
-    console.log(response);
-  });
+    console.log('weather', response);
+    lat = response.city.coord.lat
+    lon = response.city.coord.lon
+    uvIndex(lat, lon)
+});
 }
+
+function uvIndex(lat, lon) {
+    days = 5;
+    var queryURL =
+      queryBase + "uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+      console.log('queryURL', queryURL)
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log('uvi', response);
+    });
+  }
+  
+
 $('#search').click(function(){
 city = $('#searchInput').val()
 fiveDayForecast(city)
